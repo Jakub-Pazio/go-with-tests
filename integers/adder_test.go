4116828bb19753e2c5e941f588f61f98fc0e4aaf
Jakub-Pazio <19jakubpazio99@gmail.com>
2024-02-16 14:33:11 +0100

    chapter2 and godocs

--- integers/adder_test.go
+++ integers/adder_test.go
@@ -1,6 +1,9 @@
 package integers
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestAdder(t *testing.T) {
 	got := Adder(2, 2)
@@ -9,4 +12,10 @@ func TestAdder(t *testing.T) {
 	if got != want {
 		t.Errorf("wanted %d but got %d", want, got)
 	}
+}
+
+func ExampleAdder() {
+	sum := Adder(1000, 337)
+	fmt.Println(sum)
+	// Output: 1337
 }
